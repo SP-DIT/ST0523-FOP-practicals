a4304b037be957d9ff84ad94caed6dadec3d4ab4
Jeremiah Ang <jeremiah_ang@sp.edu.sg>
2025-04-14 08:54:38 +0800

    Add solution to set1

--- 1Introduction/q1.js
+++ 1Introduction/q1.js
@@ -6,7 +6,7 @@
  */
 
 let x = 4; // e.g. Create a variable named "x", initialize its value to 4
-let x1 = 5;
-console.log(x);
-console.log(typeof x);
-console.log(typeof x1);
+let x1 = 5; // e.g. Create a variable named "x1", initialize its value to 5
+console.log(x); // e.g. Get the value of x and print it to the console
+console.log(typeof x); // e.g. Get the data type of x and print it to the console
+console.log(typeof x1); // e.g. Get the data type of x and print it to the console
