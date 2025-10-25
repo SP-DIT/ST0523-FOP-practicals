8431efa21350417a75a212d511aef48e54e8d2c3
Jeremiah Ang <jeremiah_ang@sp.edu.sg>
2025-10-25 15:02:45 +0800

    Add problem 10: Fibonacci Sequence - recursive solution with testcases and code template

---
+++ 3Repetition/q10/solution.js
@@ -0,0 +1,14 @@
+function fibonacci(n) {
+    // Base cases
+    if (n === 1) {
+        return 0;
+    }
+    if (n === 2) {
+        return 1;
+    }
+    
+    // Recursive case: fibonacci(n) = fibonacci(n-1) + fibonacci(n-2)
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+module.exports = fibonacci;
