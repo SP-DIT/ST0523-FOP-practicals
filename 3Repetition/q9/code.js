272c3e82568b778a6d223071c8a6d273f01301b0
Jeremiah Ang <jeremiah_ang@sp.edu.sg>
2025-10-25 15:00:49 +0800

    Add problem 9: Sum of Digits - testcases, solution, and code template

---
+++ 3Repetition/q9/code.js
@@ -0,0 +1,7 @@
+function digitSum(n, numberOfDigits) {
+    // your code here
+}
+
+console.log(digitSum(12345, 5)); // 15
+console.log(digitSum(9876, 4)); // 30
+console.log(digitSum(1001, 4)); // 2
