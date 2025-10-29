5886b13551cfaf6439327180a07f9c01609c9249
Jeremiah Ang <jeremiah_ang@sp.edu.sg>
2025-10-29 16:01:40 +0800

    Add practical 4 question 8: Calculate Differences from Maximum with testcases and solution using absolute difference

---
+++ 4Array/q8/code.js
@@ -0,0 +1,11 @@
+function differencesFromMax(array) {
+    // your code here
+}
+
+module.exports = differencesFromMax;
+
+// examples
+
+console.log(differencesFromMax([1, 5, 3, 9, 2])); // [8, 4, 6, 0, 7] (differences from max value 9)
+console.log(differencesFromMax([10, 10, 10])); // [0, 0, 0] (all elements equal the max)
+console.log(differencesFromMax([3, 1, 4, 1, 5])); // [2, 4, 1, 4, 0] (differences from max value 5)
