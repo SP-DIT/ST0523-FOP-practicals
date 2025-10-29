63d6faf366249a5901cbdfc1b7e1f654e8daefc9
Jeremiah Ang <jeremiah_ang@sp.edu.sg>
2025-10-29 16:07:18 +0800

    Add practical 4 question 9: Analyze Array Pattern with testcases and solution using wishful thinking approach

---
+++ 4Array/q9/code.js
@@ -0,0 +1,10 @@
+function checkArray(array) {
+    // your code here
+}
+
+console.log(checkArray([1, 2, 3, 4, 5])); // 'Ascending'
+console.log(checkArray([5, 4, 3, 2, 1])); // 'Descending'
+console.log(checkArray([3, 3, 3, 3])); // 'Constant'
+console.log(checkArray([1, 1, 2, 2, 3])); // 'Non-descending'
+console.log(checkArray([5, 5, 4, 4, 3])); // 'Non-ascending'
+console.log(checkArray([1, 3, 2, 4])); // 'Unsorted'
