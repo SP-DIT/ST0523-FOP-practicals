0b41ae2cc036dc3b641d56618d085198affe14d8
Jeremiah Ang <jeremiah_ang@sp.edu.sg>
2025-10-29 15:50:55 +0800

    Add practical 4 question 5: Filter Positive Numbers with testcases and solution

---
+++ 4Array/q5/code.js
@@ -0,0 +1,10 @@
+function getPositiveNumbers(array) {
+    // your code here
+}
+
+module.exports = getPositiveNumbers;
+
+// examples
+
+console.log(getPositiveNumbers([1, -3, 5, -2, 7])); // [1, 5, 7]
+console.log(getPositiveNumbers([-5, -1, -9])); // []
