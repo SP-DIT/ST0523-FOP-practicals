e1d01c3d629ac8582cdf4235fb3743b22b3f776e
Jeremiah Ang <jeremiah_ang@sp.edu.sg>
2025-11-05 16:37:46 +0800

    Add missing import

--- 4Array/q9/code.js
+++ 4Array/q9/code.js
@@ -2,6 +2,8 @@ function checkArray(array) {
     // your code here
 }
 
+module.exports = checkArray;
+
 console.log(checkArray([1, 2, 3, 4, 5])); // 'Ascending'
 console.log(checkArray([5, 4, 3, 2, 1])); // 'Descending'
 console.log(checkArray([3, 3, 3, 3])); // 'Constant'
