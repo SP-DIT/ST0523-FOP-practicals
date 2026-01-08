24c069657c046e2a9e68170a4c5a8b32db800811
Jeremiah Ang <jeremiah_ang@sp.edu.sg>
2026-01-08 13:18:19 +0800

    Standardize Pokémon type casing in example usages

--- 5Objects/q7/code.js
+++ 5Objects/q7/code.js
@@ -8,6 +8,6 @@ module.exports = getOverallTypeEffectiveness;
 
 // examples
 
-console.log(getOverallTypeEffectiveness('Electric', ['water', 'flying'], typeChart)); // 4. Electric is 2x against Water and 2x against Flying, so 2 * 2 = 4
-console.log(getOverallTypeEffectiveness('Fire', ['grass', 'rock'], typeChart)); // 1. Rock is 0.5x against Fire and Grass is 2x against Fire, so 0.5 * 2 = 1
-console.log(getOverallTypeEffectiveness('Fighting', ['normal', 'ghost'], typeChart)); // 0. Fighting is 2x against Normal and 0x against Ghost, so 2 * 0 = 0
+console.log(getOverallTypeEffectiveness('electric', ['water', 'flying'], typeChart)); // 4. Electric is 2x against Water and 2x against Flying, so 2 * 2 = 4
+console.log(getOverallTypeEffectiveness('fire', ['grass', 'rock'], typeChart)); // 1. Rock is 0.5x against Fire and Grass is 2x against Fire, so 0.5 * 2 = 1
+console.log(getOverallTypeEffectiveness('fighting', ['normal', 'ghost'], typeChart)); // 0. Fighting is 2x against Normal and 0x against Ghost, so 2 * 0 = 0
